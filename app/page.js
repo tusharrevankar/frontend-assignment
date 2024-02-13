970531269839c292218506dd3fdfcf1496667728
tusharrevankar <tusharrevankar@gmail.com>
2024-02-13 09:18:28 +0530

    prod

--- app/page.js
+++ app/page.js
@@ -9,8 +9,7 @@ export default function Home() {
   const [randomCats, setRandomCats] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+
 
   const fetchRandomCats = async () => {
     try {
@@ -24,20 +23,7 @@ export default function Home() {
     }
   };
 
-  const handleSearch = async () => {
-    if (searchTerm.trim() === '') {
-      setSearchResults([]);
-      return;
-    }
 
-    try {
-      const response = await axios.get(`https://api.thecatapi.com/v1/images/search?has_breeds=1&q=${searchTerm}`);
-      setSearchResults(response.data);
-    } catch (error) {
-      console.error('Error fetching search results:', error);
-      setSearchResults([]);
-    }
-  };
 
   useEffect(() => {
     fetchRandomCats();
@@ -59,55 +45,23 @@ export default function Home() {
       <Slider randomCats={randomCats} isLoading={isLoading} isError={isError} />
 
       <div className="text-center p-10">
-        <h1 className="font-bold text-4xl mb-4">Search Cats</h1>
-        <div className="relative inline-block text-left">
-          <input
-            type="text"
-            onChange={(e) => setSearchTerm(e.target.value)}
-            value={searchTerm}
-            className="w-64 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-            placeholder="Search for cats..."
-          />
-          <button
-            onClick={handleSearch}
-            className="absolute right-0 top-0 h-full px-3 border-l border-gray-300 rounded-r-md bg-gray-100 hover:bg-gray-200 focus:outline-none"
-          >
-            Search
-          </button>
-          {searchResults.length > 0 && (
-            <div className="absolute mt-1 w-64 bg-white border border-gray-300 rounded-md shadow-lg z-10">
-              {searchResults.map((cat, index) => (
-                <div
-                  key={index}
-                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                >
-                  <img
-                    src={cat.url}
-                    alt={`Cat ${index + 1}`}
-                    className="h-20 w-20 object-cover rounded-md mr-2"
-                  />
-                  <span className="text-gray-800">{`Cat ${index + 1}`}</span>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+
+
       </div>
 
-
       <div className="text-center p-10">
-        <h1 className="font-bold text-4xl mb-4">Cats </h1>
+      <h1 className="font-bold text-4xl mb-4">Cat Information</h1>
       </div>
 
       <div class="flex w-full md:max-w-xl mx-4 rounded shadow">
         <a href="#" aria-current="false"
           class="w-full flex justify-center font-medium rounded-l px-5 py-2 border bg-white text-gray-800 border-gray-200 hover:bg-gray-100">
-          Grid
+          Meow
         </a>
 
         <a href="#" aria-current="page"
           class="w-full flex justify-center font-medium px-5 py-2 border-t border-b bg-gray-900 text-white  border-gray-900 hover:bg-gray-800">
-          Gallery
+          Pet
         </a>
 
         <a href="#" aria-current="false"
@@ -124,9 +78,9 @@ export default function Home() {
           </svg>
         </a>
       </div>
-
+   
       <div className="text-center p-10">
-        <h1 className="font-bold text-4xl mb-4">Cat Information</h1>
+        
         <section className="w-fit mx-auto grid grid-cols-3 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
           {isLoading ? (
             <p>Loading...</p>
@@ -140,29 +94,7 @@ export default function Home() {
                   alt={`Cat ${index + 1}`}
                   className="h-80 w-72 object-cover rounded-t-xl"
                 />
-                {/* <div className="px-4 py-3 w-72">
-                  <p className="text-lg font-bold text-black truncate block capitalize">Cat Name</p>
-                  <div className="flex items-center">
-                    <div className="ml-auto">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        className="bi bi-bag-plus"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                        />
-                        <path
-                          d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div> */}
+              
               </div>
             ))
           )}
